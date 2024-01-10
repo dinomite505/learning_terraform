0d209c71a7df3612e98241fdd3c0d45972cf2e97
dinomite505 <dino.milje@gmail.com>
2024-01-10 12:36:12 +0100

    Added a provider

--- 002_provisioners/main.tf
+++ 002_provisioners/main.tf
@@ -2,9 +2,19 @@ terraform {
     cloud {
         organization = "cloud505"
 
-    workspaces {
-        name = "provisioners"
+        workspaces {
+            name = "provisioners"
+            }
+    }
+
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.31.0"
         }
     }
-}
 
+    required_version = ">= 1.2.0"
+
+}
